ae19b5e1538781698c902696ff83b3cad219cef1
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2025-02-15 11:12:35 -0300

    grafico do ex2 corrigido

--- estudos/aula-01/ex_2.py
+++ estudos/aula-01/ex_2.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-2, 3, 100)
-y = - 4*x**2 - 6*x + 1
+y = - 4*x**2 + 6*x + 1
 
-plt.plot(x, y, label="f(x) = - 4x**2 -6x -1")
+plt.plot(x, y, label="f(x) = - 4x**2 +6x -1")
 plt.axhline(0, color='black', linewidth=0.5)
 plt.axvline(0, color='black', linewidth=0.5)
 plt.legend()
