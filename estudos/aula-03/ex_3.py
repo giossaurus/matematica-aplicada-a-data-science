116301e5af07cd70b97e21b611ae90ab8519fe08
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2025-02-20 21:58:27 -0300

    ex da aula 3

--- estudos/aula-03/ex_3.py
+++ estudos/aula-03/ex_3.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 4, 100)
 
-y = 1 * (0,4 ** x)
+y = 1 * (0.4 ** x)
 
-plt.plot(x, y, label='f(x) = 1 * 0,4^x')
+plt.plot(x, y, label='f(x) = 1 * 0.4^x')
 plt.axhline(0, color='black', linewidth=0.5)
 plt.axvline(0, color='black', linewidth=0.5)
 
-plt.title("Gráfico da função exponencial: f(x) = 1 * 0,4^x")
+plt.title("Gráfico da função exponencial: f(x) = 1 * 0.4^x")
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.legend()
